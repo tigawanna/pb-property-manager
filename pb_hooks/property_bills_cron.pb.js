36a3e5162b0e21c11d1b9f30b5b9babe1d2c5790
tigawanna <denniskinuthiawaweru@gmail.com>
2024-12-02 16:21:20 +0300

    fix: correct month and year calculations in billing cron job

--- pb_hooks/property_bills_cron.pb.js
+++ pb_hooks/property_bills_cron.pb.js
@@ -6,26 +6,24 @@
 cronAdd("bumpBillsEveryMonth", "5 0 1 * *", () => {
   const currentYear = new Date().getFullYear();
   const currentMonth = new Date().getMonth() + 1;
-  const previousMonth = currentMonth === 12 ? 1 : currentMonth - 1;
-  //  the previous yer will be the current month unless if the current month is december
+  const previousMonth = currentMonth === 1 ? 12 : currentMonth - 1;
+  // The previous year will be the current year unless the current month is January
   const previousYear = currentMonth === 1 ? currentYear - 1 : currentYear;
-
-function getPeriodBill() {
+  function getPeriodBill() {
     const records = $app.dao().findRecordsByFilter(
       "property_bills", // collection
       "month = {:month} && year = {:year}", // filter
       "-created", // sort
       500, // limit
       0, // offset
-      { month: previousMonth, year: previousYear } // optional filter params
+      { month: previousMonth - 1, year: previousYear } 
     );
     return records;
   }
 
-  console.log("=========== bumpBillsEveryMonth cron running ============");
   const bills = getPeriodBill();
   for (const bill of bills) {
-    if (!bill) return;
+    if (!bill) continue;
     const collection = $app.dao().findCollectionByNameOrId("property_bills");
     const record = new Record(collection);
     const form = new RecordUpsertForm($app, record);
@@ -34,14 +32,13 @@ function getPeriodBill() {
       shop: bill.get("shop"),
       elec_readings: bill.get("elec_readings"),
       water_readings: bill.get("water_readings"),
-      // if current month is 12 then readinsg should be for month 11 and 10 was the latest readings we have so we'll carry forward all the 10 readings to 11
-      // in case there are no readings ( shop locked or something ) fro this month making sure prev value won't be undifined next month
+      // If the current month is December, the readings should be for month 11, and month 10 was the latest readings we have, so we'll carry forward all the readings from month 10 to 11
+      // In case there are no readings (e.g., shop locked) for this month, ensure the previous value won't be undefined next month
       month: previousMonth,
-      year: currentYear,
+      year: previousYear,
     };
     console.log("============= new record ============= ", JSON.stringify(newRecord, null, 2));
     form.loadData(newRecord);
     form.submit();
   }
-  //   console.log(" ============ bills =============== ", JSON.stringify(bills));
 });
